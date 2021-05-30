dcc992d0de21a0dcbe667efc08f90c20b43ab38c
JoSihun <kepler452k@naver.com>
2021-05-31 02:03:01 +0900

    2021.05.31(월) / 02:02

--- AutoTraderV2_Temp.py
+++ AutoTraderV2_Temp.py
@@ -9,60 +9,16 @@ import numpy as np
 import pandas as pd
 import requests, pyupbit
 
-class Coin:
-    num_coins = 0
-    prices_all = pd.DataFrame()
+def get_time_header():
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    month = months[time.localtime().tm_mon - 1]
 
-    @classmethod
-    def buy(cls):
-        Coin.num_coins += 1
+    date = time.strftime(f'%a, %d {month} %Y')
+    now = time.strftime('%H:%M:%S')
+    timezone = time.strftime('%z')
+    header = f'[{date}, {now} GMT{timezone}]'
 
-    def sell(self):
-        Coin.num_coins -= 1
-
-    @classmethod
-    def get_time_str(cls):
-        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        month = months[time.localtime().tm_mon - 1]
-
-        date = time.strftime(f'%a, %d {month} %Y')
-        now = time.strftime('%H:%M:%S')
-        timezone = time.strftime('%z')
-        str_now = f'[{date}, {now} GMT{timezone}]'
-
-        return str_now
-
-    @classmethod
-    def get_prices_all(cls):
-        url = 'https://api.upbit.com/v1/market/all'
-        response = requests.get(url).json()
-
-        dataframe = pd.DataFrame()
-        for i, item in enumerate(response):
-            if item['market'].startswith('KRW'):
-                url = 'https://api.upbit.com/v1/ticker?markets=' + item['market']
-                response = requests.get(url).json()
-                print(f'{cls.get_time_str()} {url}')
-
-                new_dataframe1 = pd.DataFrame([item])
-                new_dataframe2 = pd.DataFrame(response)[['trade_price', 'high_price', 'low_price', 'acc_trade_volume', 'signed_change_rate']]
-                new_dataframe = pd.concat([new_dataframe1, new_dataframe2], axis=1, ignore_index=False)
-
-                dataframe = pd.concat([dataframe, new_dataframe], axis=0, ignore_index=True)
-                time.sleep(0.07)
-            if i % 10 == 0:
-                os.system('cls')
-                print(f'{cls.prices_all[:20]}')
-        dataframe = dataframe.sort_values(by=['signed_change_rate'], ascending=False)   # 전일대비기준 내림차순 정렬
-        dataframe['signed_change_rate'] = (dataframe['signed_change_rate'] * 100)       # 전일대비 %표기
-        dataframe = dataframe[['market', 'english_name', 'high_price', 'low_price', 'trade_price', 'signed_change_rate', 'acc_trade_volume', 'korean_name']]
-        dataframe.columns = ['MARKET_CODE', 'NAME_ENG', 'MAX_PRICE', 'MIN_PRICE', 'PRICE_NOW', 'RATE', 'VOLUME', 'NAME_KOR']
-        dataframe.reset_index(drop=True, inplace=True)      # drop: 기존 인덱스 재배열, inplace: 새 변수에 넣을 필요 없음
-        cls.prices_all = dataframe
-
-        return dataframe
-
+    return header
 
 PERCHASE_PRICE = 100000     # 종목당 실거래금액
 SECRET_KEY = 'PSrJSoS0xeQE3QlJ45pBxSSwVyZxXXRGafiBr6ZM'
@@ -73,10 +29,32 @@ if __name__ == "__main__":
     pd.set_option('display.width', None)
     upbit = pyupbit.Upbit(SECRET_KEY, ACCESS_KEY)
 
-    while True:
-        Coin.get_prices_all()
 
-        if Coin.num_coins < 20:
-            Coin.buy()
+    urlMarket = 'https://api.upbit.com/v1/market/all'
+    resMarket = requests.get(urlMarket).json()
+
+    dfMarket = pd.DataFrame(columns=['market', 'english_name', 'korean_name'])
+
+    for market in resMarket:
+        if market['market'].startswith('KRW') == True:
+            urlCoin = 'https://api.upbit.com/v1/ticker?markets=' + market['market']
+            resCoin = requests.get(urlCoin).json()
+            dfNew2 = pd.DataFrame(resCoin)[['trade_price', 'high_price', 'low_price', 'acc_trade_volume', 'signed_change_rate']]
+            dfNew1 = pd.DataFrame([market])
+            dfNew = pd.concat([dfNew1, dfNew2], axis=1, ignore_index=False)
+            time.sleep(0.07)
+
+            if (dfMarket['market'] == market['market']).any():
+                dfMarket[dfMarket['market'] == market['market']] = dfNew
+                print('True')
+            else:
+                dfMarket = pd.concat([dfMarket, dfNew], axis=0, ignore_index=True)
+        dfMarket = dfMarket.sort_values(by=['signed_change_rate'], ascending=False)  # 내림차순 정렬
+        #dfMarket['signed_change_rate'] = (dfMarket['signed_change_rate'] * 100)  # 전일대비 %표기
+        dfMarket = dfMarket[['market', 'english_name', 'trade_price', 'signed_change_rate', 'acc_trade_volume', 'korean_name']]
+        #dfMarket.columns = ['MARKET_CODE', 'NAME_ENG', 'PRICE_NOW', 'RATE', 'VOLUME', 'NAME_KOR']
+        dfMarket.reset_index(drop=True, inplace=True)  # drop: 기존 인덱스 재배열, inplace: 새 변수에 넣을 필요 없음
 
 
+        os.system('cls')
+        print(dfMarket[:20])
--- Testing.py
+++ Testing.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pandas as pd
+import requests
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-index = ['BUY', 'HOLD', 'SELL']
-columns = ['NAME_KOR', 'CODE', 'PRICE_BUY', 'RATE_YESTERDAY', 'MAX_PRICE', 'PRICE_NOW', 'MIN_PRICE', 'PROFIT', 'VOLUME', 'NAME_ENG']
-df = pd.DataFrame(values, index, columns)
+
+url = 'https://api.upbit.com/v1/market/all'
+response = requests.get(url).json()
+
+
+
+df = pd.DataFrame(columns=['MARKET_CODE', 'NAME_ENG', 'NAME_KOR'])
+for market in response:
+    if market['market'].startswith('KRW') == True:
+        df['MARKET_CODE'] = market['market']
+        df['NAME_ENG'] = market['english_name']
+        df['NAME_KOR'] = market['korean_name']
 print(df)
